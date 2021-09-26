2c1e162b8203d7d840f967d29a9a06053bd8e434
EdwinGrisalesCalle <edwingrisalescalle@gmail.com>
2021-09-26 15:18:18 -0500

    Estilos base del proyecto

---
+++ AlmacenRopa/AlmacenRopa/Scripts/CustomScript.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
+
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+    indicators: true;
+
+    //fullWidth: true;
+
+    setInterval(function () {
+        $('.carousel').carousel('next');
+    }, 1500);
+});
