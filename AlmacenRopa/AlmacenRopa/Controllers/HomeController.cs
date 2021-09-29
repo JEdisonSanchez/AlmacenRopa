f11cf27b6fd7529e36ccb77778cbe6d4741f355d
EdwinGrisalesCalle <edwingrisalescalle@gmail.com>
2021-09-28 19:33:23 -0500

    Logica Login Validacion de Inicio de sesion segun el rol del Usuario

--- AlmacenRopa/AlmacenRopa/Controllers/HomeController.cs
+++ AlmacenRopa/AlmacenRopa/Controllers/HomeController.cs
@@ -1,13 +1,62 @@
-﻿using System;
+﻿using AlmacenRopa.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace AlmacenRopa.Controllers
 {
     public class HomeController : Controller
     {
+        private CLOTHINGSTOREEntities db = new CLOTHINGSTOREEntities();
+
+
+        [HttpPost]
+        public string Login(string txtUsuario, string txtPassword)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Formatting = Formatting.Indented
+            };
+            settings.Converters.Add(new StringEnumConverter());
+
+
+            var datosUser = db.C_USER.Select(a => a).Where(x => x.SESION_NAME == txtUsuario && x.SESION_PASSWORD == txtPassword).FirstOrDefault();
+
+            if (datosUser != null)
+            {
+                FormsAuthentication.SetAuthCookie(datosUser.SESION_NAME, false);
+                HttpContext.Session.Add("usuario", datosUser.SESION_NAME);
+                HttpContext.Session.Add("rol", datosUser.ROLE_USER.NAME_ROLE);
+                HttpContext.Session.Add("id", datosUser.IDUSER);
+
+                var json = JsonConvert.SerializeObject(datosUser, settings);
+
+                //return Newtonsoft.Json.JsonConvert.DeserializeObject(datosUser);
+                return Newtonsoft.Json.JsonConvert.SerializeObject(json);
+
+            }
+            else
+
+                return "{\"error\":\"Datos Erroneos\"}";
+
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("index");
+        }
+
+
         public ActionResult Index()
         {
             return View();
--- AlmacenRopa/AlmacenRopa/Scripts/CustomScript.js
+++ AlmacenRopa/AlmacenRopa/Scripts/CustomScript.js
@@ -18,3 +18,28 @@ $(document).ready(function () {
         $('.carousel').carousel('next');
     }, 1500);
 });
+
+
+function logearUsuario() {
+    $.ajax({
+        "url": "http://localhost:62716/Home/Login",
+        "type": "post",
+        "data": {
+            'txtUsuario': $('#txtUsuario').val(),
+            'txtPassword': $('#txtPassword').val()
+        },
+        "dataType": "json",
+        "success": function (datos) {
+            if (datos.error) {
+                M.toast({ html: 'Datos Erroneos' });
+            } else {
+                M.toast({ html: 'Datos Correctos' });
+                window.location.href = 'http://localhost:62716/Product/Index';
+            }
+        },
+        "error": function () {
+            //Hacer algo
+            alert("Error!");
+        }
+    });
+}
